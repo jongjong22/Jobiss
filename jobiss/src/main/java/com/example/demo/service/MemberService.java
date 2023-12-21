938ff06c7a22c28c7f84a38e050d7602b5742bff
HyunBaeL <seehb126@naver.com>
2023-12-21 14:46:55 +0900

    환경설정 수정

--- jobiss/src/main/java/com/example/demo/service/MemberService.java
+++ jobiss/src/main/java/com/example/demo/service/MemberService.java
@@ -1,12 +1,9 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.MemberDao;
-import com.example.demo.model.Member;
 
 @Service
 public class MemberService {
@@ -14,29 +11,8 @@ public class MemberService {
 	@Autowired
 	private MemberDao dao;
 
- 
-
-	public List<Member> memberList() {
-		return dao.memberList();
-	}
-
-
-
-	public Member selectMemberOne(int id) {
-		return dao.selectMemberOne(id);
-	}
-
-
-
 	public int count() {
 		return dao.count();
 	}
-
-
-
-	public int updateMember(Member member) {
-		return dao.updateMember(member);
-	}
-
 
 }
