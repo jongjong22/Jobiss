ed1660864f81788e4c39c37331d4f3766232f87e
오창근 <139185142+Ohchanggeun@users.noreply.github.com>
2023-12-26 14:45:01 +0900

    마이페이지 현재 작업중, 회원수정 xml (현재 불가능)

--- jobiss/src/main/java/com/example/demo/controller/MemberController.java
+++ jobiss/src/main/java/com/example/demo/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.model.Member;
+import com.example.demo.model.QnA;
+import com.example.demo.model.Review;
 import com.example.demo.service.MemberService;
 
 @Controller
@@ -19,7 +21,6 @@ public class MemberController {
 
 	@Autowired
 	private MemberService service;
-	
 
 	// 로그인 페이지로 이동
 	@RequestMapping("loginform.do")
@@ -35,28 +36,28 @@ public class MemberController {
 
 		System.out.println(memail);
 		System.out.println("로그인 버튼을 눌렀음");
-		
+
 		Member dbmember = service.dbselectone(memail);
-		
-		if(dbmember != null && dbmember.getMpw().equals(mpw)) {
-			session.setAttribute("member", dbmember);				// member로 세션설정
+
+		if (dbmember != null && dbmember.getMpw().equals(mpw)) {
+			session.setAttribute("member", dbmember); // member로 세션설정
 			return "Y";
-		}else {
+		} else {
 			return "N";
 		}
-		
+
 	}
+
 	// 로그아웃
 	@GetMapping("logout.do")
 	public String logout(HttpSession session) {
 		System.out.println("로그아웃 버튼 누름");
-		
+
 		session.invalidate();
-		
-		return"common/main";
+
+		return "common/main";
 	}
-	
-	
+
 	// 회원가입 폼으로 이동
 	@RequestMapping("logininsertform.do")
 	public String mbrinsertform(Model model) {
@@ -68,12 +69,77 @@ public class MemberController {
 	@RequestMapping("logininsert.do")
 	public String logininsert(Model model, Member member) {
 
-
 		int result = service.logininsert(member);
-		
+
 		model.addAttribute("result", result);
-		
+
 		return "member/logininsert";
 	}
 
+	// 회원정보 수정 이동(마이페이지 내에서 가능)
+	@RequestMapping("memberupdateform.do")
+	public String memberupdateform(Model model, Member member, HttpSession session) {
+
+		member = (Member) session.getAttribute("member");
+
+		model.addAttribute("member", member);
+
+		return "member/mypage/memberupdateform";
+	}
+
+	// 회원정보 수정
+	@RequestMapping("memberupdate.do")
+	public String memberupdate(Model model, Member member, HttpSession session) {
+		
+		int result = 0;
+		
+		result = service.updatemember(member);
+		
+		model.addAttribute("result", result);
+//		String gender = member.getMgender();
+//		System.out.println("gender : " + gender);
+		return "member/mypage/memberupdate";
+	}
+
+	// 마이페이지 이동
+	@RequestMapping("mypage.do")
+	public String mypage(Model model, HttpSession session) {
+
+		Member member = (Member) session.getAttribute("member");
+
+		String memail = member.getMemail();
+
+		System.out.println("memail: " + memail);
+
+		return "member/mypage/mypage";
+	}
+
+	// 마이페이지 리뷰 이동
+	@RequestMapping("myreview.do")
+	public String myreview(Model model, HttpSession session, Review review) {
+
+		return "member/mypage/myreview";
+	}
+
+	// 마이페이지 Qna 이동
+	@RequestMapping("myqna.do")
+	public String myqna(Model model, HttpSession session, QnA qna) {
+
+		return "member/mypage/myqna";
+	}
+
+	// 마이페이지 피드백 이동
+	@RequestMapping("myfeedback.do")
+	public String myfeedback(Model model, HttpSession session) {
+
+		return "member/mypage/myfeedback";
+
+	}
+
+	// 마이페이지 커뮤니티 이동
+	@RequestMapping("mycommunity.do")
+	public String mycommunity(Model model, HttpSession session) {
+
+		return "member/mypage/mycommunity";
+	}
 }
--- jobiss/src/main/java/com/example/demo/dao/MemberDao.java
+++ jobiss/src/main/java/com/example/demo/dao/MemberDao.java
@@ -12,6 +12,9 @@ public interface MemberDao {
 	
 	// 1명의 회원정보
 	Member dbselectone(String memail);
+	
+	// 회원수정
+	int updatemember(Member member);
 
 
 }
