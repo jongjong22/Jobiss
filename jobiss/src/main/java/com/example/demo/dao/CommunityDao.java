ae706a6282e72257050b6a737a1e8147159f8c25
HyunBaeL <seehb126@naver.com>
2024-01-02 13:55:18 +0900

    커뮤니티 게시판 글작성

--- jobiss/src/main/java/com/example/demo/dao/CommunityDao.java
+++ jobiss/src/main/java/com/example/demo/dao/CommunityDao.java
@@ -1,8 +1,18 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.Community;
 
 @Mapper
 public interface CommunityDao {
+
+	// 커뮤니티 글 insert
+	int communityInsert(Community community);
+
+	// 전체 커뮤니티 글 리스트 구하기
+	List<Community> selectCommunityList();
 
 }
